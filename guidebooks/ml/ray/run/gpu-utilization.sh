7aee595122695ec1a6ae3315f3e8fb5025e892e1
Nick Mitchell <nickm@us.ibm.com>
2022-06-14 21:10:59 -0400

    feat: add memory utilization to gpu utilization guidebook
    This PR also improves the formatting of the output:
    
    1) include a timestamp with every data point (to improve chart-ability of data)
    2) include the name of the pod (to improve handlng of multi-pod runs)
    3) 0 % Utilization Gpu, versus Gpu Utilization     0 %
    4) show important information in bold to make it jump out even more

--- guidebooks/ml/ray/run/gpu-utilization.sh
+++ guidebooks/ml/ray/run/gpu-utilization.sh
@@ -1,3 +1,3 @@
 kubectl get pod -l ${KUBE_POD_LABEL_SELECTOR} --context ${KUBE_CONTEXT} -n ${KUBE_NS} -o name \
-    | xargs -I {} -n1 kubectl exec --context ${KUBE_CONTEXT} -n ${KUBE_NS} {} -- \
-            sh -c "nvidia-smi -q -d UTILIZATION -lms 5000 | awk -W interactive -F '           :' '/Gpu/ {print \"\n\033[31m\" \"Gpu Utilization:\", \$2 \"\033[0m\"}'" | tee "${STREAMCONSUMER_RESOURCES}gpu.txt"
+    | xargs -P0 -I {} -n1 kubectl exec --context ${KUBE_CONTEXT} -n ${KUBE_NS} {} -- \
+            sh -c "nvidia-smi -q -d UTILIZATION -lms 5000 | awk -W interactive -v IDX={} -F : '\$1 ~ /Timestamp/ { ts=\$2 \":\" \$3 \":\" \$4 } /(Gpu|Memory).+%$/ {sub(/^ +/, \"\", \$1); print \"\033[31;1m\" \$2, \"Utilization\", \$1, \"\033[0m\", IDX, ts}'"
