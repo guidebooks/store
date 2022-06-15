254282087e4f26718a4ae0169115659660e753b3
Nick Mitchell <nickm@us.ibm.com>
2022-06-15 10:20:54 -0400

    feat: add gpu total memory to gpu utilization stream

--- guidebooks/ml/ray/run/gpu-utilization.sh
+++ guidebooks/ml/ray/run/gpu-utilization.sh
@@ -1,3 +1,3 @@
 kubectl get pod -l ${KUBE_POD_LABEL_SELECTOR} --context ${KUBE_CONTEXT} -n ${KUBE_NS} -o name \
     | xargs -P0 -I {} -n1 kubectl exec --context ${KUBE_CONTEXT} -n ${KUBE_NS} {} -- \
-            sh -c "nvidia-smi -q -d UTILIZATION -lms 5000 | awk -W interactive -v IDX={} -F : '\$1 ~ /Timestamp/ { ts=\$2 \":\" \$3 \":\" \$4 } /(Gpu|Memory).+%$/ {sub(/^ +/, \"\", \$1); print \"\033[31;1m\" \$2, \"Utilization\", \$1, \"\033[0m\", IDX, ts}'"
+            sh -c "nvidia-smi -q -d UTILIZATION,MEMORY -lms 5000 | awk -W interactive -v IDX={} -F : '\$1 ~ /Timestamp/ { ts=\$2 \":\" \$3 \":\" \$4 } /Total/ && total==0 {total=1;sub(/^ +/, \"\", \$1); print \"\033[31;1m\" \$2, \"Memory\", \$1, \"\033[0m\", IDX, ts } /(Gpu|Memory).+%$/ {sub(/^ +/, \"\", \$1); print \"\033[31;1m\" \$2, \"Utilization\", \$1, \"\033[0m\", IDX, ts}'"
