0d0e9d326f01b15be2a1b9a31f1511939613f42a
Nick Mitchell <nickm@us.ibm.com>
2022-06-24 09:49:08 -0400

    feat: in ml/ray/run/logs, track pod vmstat info

---
+++ guidebooks/ml/ray/run/pod-vmstat.sh
@@ -0,0 +1,4 @@
+kubectl get pod -l ${KUBE_POD_LABEL_SELECTOR} --context ${KUBE_CONTEXT} -n ${KUBE_NS} -o name \
+    | xargs -P0 -I {} -n1 kubectl exec --context ${KUBE_CONTEXT} -n ${KUBE_NS} {} -- \
+            sh -c "vmstat 2 | awk -Winteractive -v pod=\$(hostname) 'FNR==1 {x=substr(pod,length(\"Hostname\") + 1); gsub(/./, \"-\", x); print \"Hostname \" x,\$0} FNR>1 {print pod,\$0}'" \
+    >> "${STREAMCONSUMER_RESOURCES}pod-vmstat.txt"
