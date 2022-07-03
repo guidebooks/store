94117921a857da3cdf07138f770491aadc207d3c
Nick Mitchell <nickm@us.ibm.com>
2022-07-03 15:57:44 -0400

    fix: stagger the starts of ml/ray/ kubectl execs to avoid storming the api server

--- guidebooks/ml/ray/run/pod-vmstat-memory.sh
+++ guidebooks/ml/ray/run/pod-vmstat-memory.sh
@@ -1,4 +1,7 @@
+# staggered starts
+sleep 0.$(shuf -i 10000-11000 -n1)
+
 kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} --context ${KUBE_CONTEXT} -n ${KUBE_NS} -o name \
-    | xargs -P128 -I {} -n1 kubectl exec --context ${KUBE_CONTEXT} -n ${KUBE_NS} {} -- \
-            sh -c "while true; do echo \"\$(hostname) \$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes) \$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)\"; sleep 10; done" \
+    | xargs -S5000 -P128 -I {} -n1 \
+            sh -c "sleep 0.\$(shuf -i 100-2000 -n1); kubectl exec --context ${KUBE_CONTEXT} -n ${KUBE_NS} {} -- sh -c \"while true; do echo \\\"\\\$(hostname) \\\$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes) \\\$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)\\\"; sleep 10; done\"" \
     >> "${STREAMCONSUMER_RESOURCES}pod-memory.txt"
--- guidebooks/ml/ray/run/pod-vmstat.sh
+++ guidebooks/ml/ray/run/pod-vmstat.sh
@@ -1,4 +1,7 @@
+# staggered starts
+sleep 0.$(shuf -i 5000-7000 -n1)
+
 kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} --context ${KUBE_CONTEXT} -n ${KUBE_NS} -o name \
-    | xargs -P128 -I {} -n1 kubectl exec --context ${KUBE_CONTEXT} -n ${KUBE_NS} {} -- \
-            sh -c "vmstat 2 --timestamp | awk -Winteractive -v pod=\$(hostname) 'FNR==1 {x=substr(pod,length(\"Hostname\") + 1); gsub(/./, \"-\", x); print \"Hostname \" x,\$0} FNR>1 {print pod,\$0}'" \
+    | xargs -S1000 -P128 -I {} -n1 \
+            sh -c "sleep 0.\$(shuf -i 100-2000 -n1); kubectl exec --context ${KUBE_CONTEXT} -n ${KUBE_NS} {} -- sh -c \"vmstat 2 --timestamp | awk -Winteractive -v pod=\\\$(hostname) 'FNR==1 {x=substr(pod,length(\\\"Hostname\\\") + 1); gsub(/./, \\\"-\\\", x); print \\\"Hostname \\\" x,\\\$0} FNR>1 {print pod,\\\$0}'\"" \
     >> "${STREAMCONSUMER_RESOURCES}pod-vmstat.txt"
