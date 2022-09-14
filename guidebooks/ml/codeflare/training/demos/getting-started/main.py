f207302247464a4f16a4e3f713cf9e48bf91c7d6
Nick Mitchell <nickm@us.ibm.com>
2022-09-14 15:10:21 -0400

    feat: add ml/codeflare/training/demos/getting-started/s3
    this is a variant of ml/codeflare/training/demos/getting-started that supports pulling the mnist.npz data from s3

--- guidebooks/ml/codeflare/training/demos/getting-started/main.py
+++ guidebooks/ml/codeflare/training/demos/getting-started/main.py
@@ -13,13 +13,45 @@
 # Attribution: https://github.com/tensorflow/tensorboard/blob/master/docs/get_started.ipynb
 # IntegratedBy: @starpit 20220724 modifications marked @starpit
 
+import os
 import tensorflow as tf
 import datetime
 
-# Using the MNIST dataset as the example, normalize the data and write a function that creates a simple Keras model for classifying the images into 10 classes.
-mnist = tf.keras.datasets.mnist
+# Optionally, we support loading the mnist.npz data from s3. This is
+# only done if the S3_ENDPOINT environment variable is set. Otherwise,
+# we use the default path for loading the mnist data -- however
+# tensorflow wants to do so.
+def load_from_s3():
+  import boto3
+  import numpy as np
+  client = boto3.client(
+    's3',
+    aws_access_key_id = os.environ.get('S3_ACCESS_KEY_ID'),
+    aws_secret_access_key = os.environ.get('S3_SECRET_ACCESS_KEY'),
+    endpoint_url = os.environ.get('S3_ENDPOINT')
+  )
 
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
+  bucket=os.environ.get('S3_BUCKET')
+  object=os.environ.get('S3_OBJECT')
+  file=os.path.join('/tmp', os.path.basename(object))
+  print(f"  Data: try remote get {object} from s3")
+  client.download_file(bucket, object, file)
+  print(f"  Data: done remote get {object} from s3")
+  print(f"  Data: try parsing")
+  with np.load(file, allow_pickle=True) as f:
+    x_train, y_train = f["x_train"], f["y_train"]
+    x_test, y_test = f["x_test"], f["y_test"]
+
+    print(f"  Data: done parsing")
+    return (x_train, y_train), (x_test, y_test)
+
+# Using the MNIST dataset as the example, normalize the data and write a function that creates a simple Keras model for classifying the images into 10 classes.
+if os.getenv("S3_ENDPOINT"):
+  (x_train, y_train),(x_test, y_test) = load_from_s3()
+else:
+  mnist = tf.keras.datasets.mnist
+  (x_train, y_train),(x_test, y_test) = mnist.load_data()
+  
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 def create_model():
