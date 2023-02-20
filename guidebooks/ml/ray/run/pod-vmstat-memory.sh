a085d2e48807d6bcd4dd3d484a3587b1a9f0a2a2
Nick Mitchell <nickm@us.ibm.com>
2023-02-19 19:53:00 -0500

    feat: improve console readability of ml/ray/run/pod-vmstat and memory stats
    BREAKING CHANGE: this breaks any clients that were assuming a normal vmstat format for the cpu stats. it is now limited to the cpu columns (i.e. us sy id wa st).

--- guidebooks/ml/ray/run/pod-vmstat-memory.sh
+++ guidebooks/ml/ray/run/pod-vmstat-memory.sh
@@ -5,11 +5,12 @@ if [ -n "$LOG_AGGREGATOR_POD_NAME" ] && [ -n "$LOG_AGGREGATOR_LOGDIR" ]; then
 fi
 
 # staggered starts
-sleep 0.$(shuf -i 10000-11000 -n1)
+# sleep 0.$(shuf -i 10000-11000 -n1)
 
 if [ $(uname) = "Darwin" ]; then export REPLSIZE="-S5000"; fi
 
 kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} -o name \
     | xargs ${REPLSIZE} -P128 -I {} -n1 \
-            sh -c "sleep 0.\$(shuf -i 100-2000 -n1); kubectl exec ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} {} -- sh -c \"while true; do echo \\\"\\\$(hostname) \\\$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.current) \\\$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.max)\\\"; sleep 10; done\"" \
-    >> "${STREAMCONSUMER_RESOURCES}pod-memory.txt"
+            sh -c "kubectl exec ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} {} -- sh -c \"while true; do echo \\\"\\\$(hostname) \\\$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.current) \\\$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.max)\\\" | awk '{printf(\\\"%s %s %s %5.2f%% [Mem Utilization]\\\n\\\", \\\$1, \\\$2, \\\$3, 100*\\\$2/\\\$3)}'; sleep 10; done\"" \
+    | sed -lE 's/^ray-.+-(ray-.+)$/\x1B[34m\1\x1B[0m/' \
+    | tee -a "${STREAMCONSUMER_RESOURCES}pod-memory.txt"
