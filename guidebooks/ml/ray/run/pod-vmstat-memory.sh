bbf2c4d474377a134168ac3e3d6354acff777b42
Nick Mitchell <nickm@us.ibm.com>
2023-02-23 14:26:39 -0500

    fix: ml/ray/run utilization info should flow to stderr

--- guidebooks/ml/ray/run/pod-vmstat-memory.sh
+++ guidebooks/ml/ray/run/pod-vmstat-memory.sh
@@ -16,4 +16,5 @@ kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} ${KUBE_CONTEXT_ARG} ${KUBE_NS_
     | xargs ${REPLSIZE} -P128 -I {} -n1 \
             sh -c "kubectl exec ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} {} -- sh -c \"while true; do echo \\\"\\\$(hostname) \\\$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.current) \\\$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.max)\\\" | awk -Winteractive -v now=\\\"\\\$(TZ=$TZ date +'%Y-%m-%d %H:%M:%S %Z')\\\" '{printf(\\\"%s %13s %13s   [Mem Utilization %5.1f%%] %s\\\n\\\", \\\$1, \\\$2, \\\$3, 100*\\\$2/\\\$3, now)}'; sleep 5; done\"" \
     | sed -lE 's/^(ray-[^-]+-[^-]+)-[a-z0-9-]+(.+)$/\x1B[34m\1\2\x1B[0m/' \
-    | tee -a "${STREAMCONSUMER_RESOURCES}pod-memory.txt"
+    | tee -a "${STREAMCONSUMER_RESOURCES}pod-memory.txt" \
+    >(cat 1>&2)
