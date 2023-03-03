e0dcb526d2cc8460a4b5883bbbe615b5cf6290f0
Nick Mitchell <nickm@us.ibm.com>
2023-03-03 18:31:19 -0500

    fix: sed invalid reference errors

--- guidebooks/ml/ray/run/pod-vmstat-memory.sh
+++ guidebooks/ml/ray/run/pod-vmstat-memory.sh
@@ -17,7 +17,7 @@ TZ=$(date +%Z)
 kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} -o name \
         --field-selector=status.phase==Running \
     | xargs ${REPLSIZE} -P128 -I {} -n1 \
-            sh -c "kubectl exec --pod-running-timeout=1h ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} {} -- sh -c \"while true; do echo \\\"\\\$(hostname) \\\$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.current) \\\$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.max)\\\" | awk -Winteractive -v now=\\\"\\\$(TZ=$TZ date +'%Y-%m-%d %H:%M:%S %Z')\\\" '{printf(\\\"%s %13s %13s   [Mem Utilization %5.1f%%] %s\\\n\\\", \\\$1, \\\$2, \\\$3, 100*\\\$2/\\\$3, now)}'; sleep 5; done\"" \
-    | sed -lE 's/^(ray-[^-]+-[^-]+)-[a-z0-9-]+(.+)$/\x1B[34m\1\2\x1B[0m/' \
+            sh -c "kubectl exec --pod-running-timeout=1h ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} {} -- sh -c \"while true; do echo \\\"\\\$(hostname) \\\$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.current) \\\$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.max)\\\" | awk -Winteractive -v now=\\\"\\\$(TZ=$TZ date +'%Y-%m-%d %H:%M:%S %Z')\\\" '{printf(\\\"\\\x1b[34m%s %13s %13s   [Mem Utilization %5.1f%%] %s\\\x1b[0m\\\n\\\", \\\$1, \\\$2, \\\$3, 100*\\\$2/\\\$3, now)}'; sleep 5; done\"" \
+    | sed -lE 's/-[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}//g' \
     | tee -a "${STREAMCONSUMER_RESOURCES}pod-memory.txt" \
     1>&2
