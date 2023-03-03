4be8f22ac75eb0748599b2193a71a87d08f2f821
Nick Mitchell <nickm@us.ibm.com>
2023-03-03 13:29:20 -0500

    fix: improved error handling in helm install for lack of cluster name
    This also improves the log output to be more consistent.

--- guidebooks/ml/ray/start/kubernetes/install-via-helm.sh
+++ guidebooks/ml/ray/start/kubernetes/install-via-helm.sh
@@ -39,16 +39,18 @@ fi
 
 # sparse clone
 if [ -n "$BRANCH" ]; then BRANCHOPT="-b $BRANCH"; fi
-echo "Cloning https://${GITHUB}/${ORG}/${REPO}.git ${BRANCHOPT}"
+echo "$(tput setaf 4)[Helm] $(tput setaf 5)Cloning https://${GITHUB}/${ORG}/${REPO}.git ${BRANCHOPT}$(tput sgr0)"
+tput setaf 3
 (git clone -q --no-checkout --filter=blob:none https://${GITHUB}/${ORG}/${REPO}.git ${BRANCHOPT} > /dev/null && \
     cd $REPO && \
     git sparse-checkout set --cone $SUBDIR && git checkout ${BRANCH-main})
+tput sgr0
 
 if [ "$KUBE_POD_MANAGER" = ray ]; then
     sed -i '' -e 's/imagePullPolicy: Always/imagePullPolicy: IfNotPresent/g' $REPO/$SUBDIR/templates/*.yaml
 fi
 
-echo "$(tput setaf 4)Creating ray cluster num_cpus=$(tput setaf 5)${NUM_CPUS-1} $(tput setaf 4)num_gpus=$(tput setaf 5)${NUM_GPUS-1} $(tput setaf 4)head_memory=$(tput setaf 5)${HEAD_MEMORY-1Gi} $(tput setaf 4)worker_memory=$(tput setaf 5)${WORKER_MEMORY-1Gi} $(tput setaf 4)minWorkers=$(tput setaf 5)${MIN_WORKERS-2} $(tput setaf 4)maxWorkers=$(tput setaf 5)${MAX_WORKERS-3} $(tput setaf 4)image=$(tput setaf 5)${RAY_IMAGE} $(tput setaf 4)operatorImage=$(tput setaf 5)${RAY_OPERATOR_IMAGE} $(tput setaf 4)context="$(tput setaf 5)${KUBE_CONTEXT_ARG_HELM}" $(tput setaf 4)namespace="$(tput setaf 5)${KUBE_NS_ARG}$(tput sgr0)" $(tput setaf 4)clusterOnly=$(tput setaf 5)${CLUSTER_ONLY}$(tput sgr0)"
+echo "$(tput setaf 4)[Helm] Creating ray cluster num_cpus=$(tput setaf 5)${NUM_CPUS-1} $(tput setaf 4)num_gpus=$(tput setaf 5)${NUM_GPUS-1} $(tput setaf 4)head_memory=$(tput setaf 5)${HEAD_MEMORY-1Gi} $(tput setaf 4)worker_memory=$(tput setaf 5)${WORKER_MEMORY-1Gi} $(tput setaf 4)minWorkers=$(tput setaf 5)${MIN_WORKERS-2} $(tput setaf 4)maxWorkers=$(tput setaf 5)${MAX_WORKERS-3} $(tput setaf 4)image=$(tput setaf 5)${RAY_IMAGE} $(tput setaf 4)operatorImage=$(tput setaf 5)${RAY_OPERATOR_IMAGE} $(tput setaf 4)context="$(tput setaf 5)${KUBE_CONTEXT_ARG_HELM}" $(tput setaf 4)namespace="$(tput setaf 5)${KUBE_NS_ARG}$(tput sgr0)" $(tput setaf 4)clusterOnly=$(tput setaf 5)${CLUSTER_ONLY}$(tput sgr0)"
 
 # sigh, ray's --num-cpus flag does not understand millicores, such as 250m, nor fractional cores at all
 # this maps 250m => 1 and 2500m => 3 and 4 => 4
@@ -73,14 +75,13 @@ fi
 OUTPUT=/tmp/helm.yml
 if [ -n "$HELM_DRYRUN" ]; then
     INSTALL="install --dry-run"
-    echo "Performing dry run"
+    echo "$(tput setaf 4)[Helm] $(tput setaf 5)Performing dry run$(tput sgr0)"
 else
     INSTALL="upgrade --install"
-    echo "Performing install"
 fi
 
 if [ -n "$KUBE_POD_SCHEDULING_PRIO" ]; then
-    echo "$(tput setaf 4)Using job priority=$(tput setaf 5)$KUBE_POD_SCHEDULING_PRIO$(tput sgr0)"
+    echo "$(tput setaf 4)[Helm] Using job priority=$(tput setaf 5)$KUBE_POD_SCHEDULING_PRIO$(tput sgr0)"
     jobPriority="--set priority=${KUBE_POD_SCHEDULING_PRIO}"
 fi
 
@@ -88,12 +89,12 @@ if [ -n "$CUSTOM_WORKING_DIR" ]; then
     if [ -d "$CUSTOM_WORKING_DIR" ]; then
         workdirEnc=$(tar -jcf - --no-xattrs --exclude '*~' --exclude '*.out' --exclude '*.log' --exclude '*.err' -C "$CUSTOM_WORKING_DIR" . | base64)
         workdir="--set workdir=${workdirEnc}"
-        echo "$(tput setaf 4)Using workdir via configmap=$(tput setaf 5)${#workdir} bytes$(tput sgr0)"
+        echo "$(tput setaf 4)[Helm] Using workdir via configmap=$(tput setaf 5)${#workdir} bytes$(tput sgr0)"
     elif [ ! -e "$CUSTOM_WORKING_DIR" ]; then
-        echo "$(tput setaf 1)Error: custom working directory specified, but path to directory not found $CUSTOM_WORKING_DIR$(tput sgr0)"
+        echo "$(tput setaf 1)[Helm] Error: custom working directory specified, but path to directory not found $CUSTOM_WORKING_DIR$(tput sgr0)"
         exit 1
     else
-        echo "$(tput setaf 1)Error: custom working directory specified, but path does not point to a directory $CUSTOM_WORKING_DIR$(tput sgr0)"
+        echo "$(tput setaf 1)[Helm] Error: custom working directory specified, but path does not point to a directory $CUSTOM_WORKING_DIR$(tput sgr0)"
         exit 1
     fi
 fi
@@ -101,12 +102,12 @@ fi
 if [ -n "$GUIDEBOOK_DASHDASH" ]; then
     dashdashEnc=$(echo "$GUIDEBOOK_DASHDASH" | base64)
     dashdash="--set dashdash=${dashdashEnc}"
-    echo "$(tput setaf 4)Using dashdash=$(tput setaf 5)${GUIDEBOOK_DASHDASH}$(tput sgr0)"
+    echo "$(tput setaf 4)[Helm] Using dashdash=$(tput setaf 5)${GUIDEBOOK_DASHDASH}$(tput sgr0)"
 fi
 
 if [ -n "$GUIDEBOOK_ENV" ]; then
     jobEnv="--set jobEnv=$GUIDEBOOK_ENV"
-    echo -n "$(tput setaf 4)Passing through job env=$(tput setaf 5)"
+    echo -n "$(tput setaf 4)[Helm] Passing through job env=$(tput setaf 5)"
     echo -n $(echo "$GUIDEBOOK_ENV" | base64 -d)
     echo "$(tput sgr0)"
 fi
@@ -115,6 +116,14 @@ if [ -n "$JOB_ID" ]; then
     jobId="--set jobId=${JOB_ID}"
 fi
 
+if [ -z "$RAY_KUBE_CLUSTER_NAME" ]; then
+    echo "$(tput setaf 1)[Helm] Error: ray cluster name not defined$(tput sgr0)"
+    exit 1
+else
+    echo "$(tput setaf 4)[Helm] Using ray cluster name=$(tput setaf 5)${RAY_KUBE_CLUSTER_NAME}$(tput sgr0)"
+fi
+
+tput setaf 3
 cd $REPO/$SUBDIR && \
     helm ${HELM_DEBUG} ${INSTALL} --wait --timeout 30m ${RAY_KUBE_CLUSTER_NAME} . \
          ${KUBE_CONTEXT_ARG_HELM} ${KUBE_NS_ARG} \
@@ -151,9 +160,10 @@ cd $REPO/$SUBDIR && \
          --set image=${RAY_IMAGE} \
          ${imagePullSecret} \
          ${imagePullPolicy} | tee ${OUTPUT}
+tput sgr0
 
 if [ -n "$HELM_DRYRUN" ]; then
     # early exit
-    echo "Helm chart should be in ${OUTPUT}"
+    echo "[Helm] yaml spec should be in ${OUTPUT}"
     exit 90
 fi
