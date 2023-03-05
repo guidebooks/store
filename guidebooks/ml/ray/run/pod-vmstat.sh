9660ec11717ca53f22629ea61651a63759bb9457
Nick Mitchell <nickm@us.ibm.com>
2023-03-04 19:57:54 -0500

    fix: avoid use of grep in pod-vmstat and kube events to improve streaming on linux

--- guidebooks/ml/ray/run/pod-vmstat.sh
+++ guidebooks/ml/ray/run/pod-vmstat.sh
@@ -17,8 +17,7 @@ TZ=$(date +%Z)
 kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} -o name \
         --field-selector=status.phase==Running \
     | xargs ${REPLSIZE} -P128 -I {} -n1 \
-            sh -c "kubectl exec --pod-running-timeout=1h ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} {} -- sh -c \"TZ=$TZ vmstat --timestamp 5 | awk -Winteractive -v timezone=$TZ -v pod=\\\$(hostname) 'FNR>2 {printf(\\\"\\\x1b[33m%s %14s %2s %2s %2s %2s %2s [CPU Utilization %5.1f%%] %s %s %s\\\x1b[0m\\\n\\\", pod, \\\$4, \\\$13, \\\$14, \\\$15, \\\$16, \\\$17, \\\$13+\\\$14, \\\$(NF-1), \\\$NF, timezone)}'\"" \
+            sh -c "kubectl exec --pod-running-timeout=1h ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} {} -- sh -c \"TZ=$TZ vmstat --timestamp 5 | awk -Winteractive -v timezone=$TZ -v pod=\\\$(hostname) '\\\$NF !~ /timestamp/ && \\\$3 != \\\"swpd\\\" {printf(\\\"\\\x1b[33m%s %14s %2s %2s %2s %2s %2s [CPU Utilization %5.1f%%] %s %s %s\\\x1b[0m\\\n\\\", pod, \\\$4, \\\$13, \\\$14, \\\$15, \\\$16, \\\$17, \\\$13+\\\$14, \\\$(NF-1), \\\$NF, timezone)}'\"" \
     | sed -lE 's/-[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}//g' \
-    | grep --line-buffered -v timestamp \
     | tee -a "${STREAMCONSUMER_RESOURCES}pod-vmstat.txt" \
     1>&2
