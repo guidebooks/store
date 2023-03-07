2d9eeea94299c501a6134b072d39a1759206747c
Nick Mitchell <nickm@us.ibm.com>
2023-03-07 15:11:14 -0500

    fix: ugh, restore awk -Winteractive

--- guidebooks/ml/ray/run/pod-vmstat.sh
+++ guidebooks/ml/ray/run/pod-vmstat.sh
@@ -9,7 +9,6 @@ fi
 
 if [ $(uname) = "Darwin" ]; then
     export REPLSIZE="-S5000"
-    export AWKOPTS="-Winteractive"
 fi
 
 if [ -z "$STREAMCONSUMER_RESOURCES" ]; then STREAMCONSUMER_RESOURCES="/tmp/"; fi
@@ -20,6 +19,6 @@ TZ=$(date +%Z)
 kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} -o name \
         --field-selector=status.phase==Running \
     | xargs ${REPLSIZE} -P128 -I {} -n1 \
-            sh -c "kubectl exec --pod-running-timeout=1h ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} {} -- sh -c \"TZ=$TZ vmstat --timestamp 5 | awk $AWKOPTS -v timezone=$TZ -v pod=\\\$(hostname | sed -E 's/-[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}//') '\\\$NF !~ /timestamp/ && \\\$3 != \\\"swpd\\\" {printf(\\\"\\\x1b[1;33m[CPU Utilization %5.1f%%] \\\x1b[0;33m%-30s \\\x1b[2m%14s %2s %2s %2s %2s %2s %s %s %s\\\x1b[0m\\\n\\\", \\\$13+\\\$14, pod, \\\$4, \\\$13, \\\$14, \\\$15, \\\$16, \\\$17, \\\$(NF-1), \\\$NF, timezone)}'\"" \
+            sh -c "kubectl exec --pod-running-timeout=1h ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} {} -- sh -c \"TZ=$TZ vmstat --timestamp 5 | awk -Winteractive -v timezone=$TZ -v pod=\\\$(hostname | sed -E 's/-[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}//') '\\\$NF !~ /timestamp/ && \\\$3 != \\\"swpd\\\" {printf(\\\"\\\x1b[1;33m[CPU Utilization %5.1f%%] \\\x1b[0;33m%-30s \\\x1b[2m%14s %2s %2s %2s %2s %2s %s %s %s\\\x1b[0m\\\n\\\", \\\$13+\\\$14, pod, \\\$4, \\\$13, \\\$14, \\\$15, \\\$16, \\\$17, \\\$(NF-1), \\\$NF, timezone)}'\"" \
     | tee -a "${STREAMCONSUMER_RESOURCES}pod-vmstat.txt" \
     1>&2
