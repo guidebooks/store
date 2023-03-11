133fcbeaba33d2e588d08c8112f30e64b86ad40d
Nick Mitchell <nickm@us.ibm.com>
2023-03-11 17:31:11 -0500

    fix: add worker index to log output

--- guidebooks/ml/torchx/run/logs.sh
+++ guidebooks/ml/torchx/run/logs.sh
@@ -1,11 +1,13 @@
 echo "👉 $(tput setaf 6)Logs will be stored in this local staging directory: $(tput bold)${LOGDIR_STAGE}$(tput sgr0)"
 
 kubectl logs -f \
-    --timestamps \
-    --ignore-errors \
-    --pod-running-timeout=3h \
-    --max-log-requests=128 \
-    ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} \
-    -l ${TORCHX_INSTANCE_LABEL} \
-    | awk '{sub(/\.[[:digit:]]+/,"",$1); printf "\x1b[2m" $1 "\x1b[0m"; for (i=2; i<=NF; i++) printf " " $i; print ""}' \
+        --prefix \
+        --timestamps \
+        --ignore-errors \
+        --pod-running-timeout=3h \
+        --max-log-requests=128 \
+        ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} \
+        -l ${TORCHX_INSTANCE_LABEL} \
+    | sed -E "s|pod/$TORCHX_INSTANCE-([0-9]+)/$TORCHX_INSTANCE-[0-9]+|Worker#\1|" \
+    | awk '{sub(/\.[[:digit:]]+/,"",$2); printf "\x1b[33;1m" $1 "\x1b[2m " $2 "\x1b[0m"; for (i=3; i<=NF; i++) printf " " $i; print ""}' \
     | tee "${STREAMCONSUMER_LOGS}job.txt"
