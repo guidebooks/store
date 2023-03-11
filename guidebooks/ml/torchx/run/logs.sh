250f486af211d1bcf150cadd5f1e567a12b0d019
Nick Mitchell <nickm@us.ibm.com>
2023-03-11 18:23:11 -0500

    fix: shorten prefix of log lines

--- guidebooks/ml/torchx/run/logs.sh
+++ guidebooks/ml/torchx/run/logs.sh
@@ -8,6 +8,7 @@ kubectl logs -f \
         --max-log-requests=128 \
         ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} \
         -l ${TORCHX_INSTANCE_LABEL} \
-    | sed -E "s|pod/$TORCHX_INSTANCE-([0-9]+)/$TORCHX_INSTANCE-[0-9]+|Worker#\1|" \
-    | awk '{sub(/\.[[:digit:]]+/,"",$2); printf "\x1b[33;1m" $1 "\x1b[2m " $2 "\x1b[0m"; for (i=3; i<=NF; i++) printf " " $i; print ""}' \
+    | sed -E "s|pod/$TORCHX_INSTANCE-([0-9]+)/$TORCHX_INSTANCE-[0-9]+|W\1|" \
+    | sed -E 's/-[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}//' \
+    | awk '{sub(/^[[:digit:]]{4}-/,"",$2); sub(/T/," ",$2); sub(/\.[[:digit:]]+/,"",$2); printf "\x1b[33;1m" $1 " \x1b[0;2m" $2 "\x1b[0m"; for (i=3; i<=NF; i++) printf " " $i; print ""}' \
     | tee "${STREAMCONSUMER_LOGS}job.txt"
