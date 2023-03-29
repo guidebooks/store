c18a4deb030b989d752eb05bb46a40d772c37c36
Nick Mitchell <nickm@us.ibm.com>
2023-03-29 15:37:31 -0400

    fix: ml/ray/start/kubernetes/events uses incorrect awk comment character

--- guidebooks/ml/ray/start/kubernetes/events.sh
+++ guidebooks/ml/ray/start/kubernetes/events.sh
@@ -13,9 +13,9 @@ while true; do
         | awk $INT -v id=$JOB_ID '
 index($2, id) > 0 {
   printf "\x1b[1;2;35m[Workers \x1b[0;35m%s\x1b[1;2;35m]\x1b[0;2;35m", $5;
-  printf " \x1b[0;2;35m%s/%s %s", tolower($1), $2, $4; // name and reason
-  for (i=6;i<=NF;i++) printf " %s", $i; // message
-  print " \x1b[0;35m" $3 "\x1b[0m"; // timestamp
+  printf " \x1b[0;2;35m%s/%s %s", tolower($1), $2, $4; # name and reason
+  for (i=6;i<=NF;i++) printf " %s", $i; # message
+  print " \x1b[0;35m" $3 "\x1b[0m"; # timestamp
   fflush();
 }' \
         | tee -a "${STREAMCONSUMER_EVENTS}kubernetes.txt" \
