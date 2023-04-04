30c9b0cf72db00cd6fce23d1653bb0459ca7ea28
Nick Mitchell <nickm@us.ibm.com>
2023-04-04 12:43:38 -0400

    fix: torchx log streamer would fail if lines contained control chars
    this also improves the smoothness of log streaming

--- guidebooks/ml/torchx/run/logs.sh
+++ guidebooks/ml/torchx/run/logs.sh
@@ -8,7 +8,7 @@ kubectl logs -f \
         --max-log-requests=128 \
         ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} \
         -l ${TORCHX_INSTANCE_LABEL} \
-    | sed -E "s|pod/$TORCHX_INSTANCE-([0-9]+)/$TORCHX_INSTANCE-[0-9]+|W\1|" \
-    | sed -E 's/-[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}//' \
-    | awk '{sub(/^[[:digit:]]{4}-/,"",$2); sub(/T/," ",$2); sub(/\.[[:digit:]]+/,"",$2); printf "\x1b[33;1m" $1 " \x1b[0;2m" $2 "\x1b[0m"; for (i=3; i<=NF; i++) printf " " $i; print ""}' \
+    | sed -uE "s|pod/$TORCHX_INSTANCE-([0-9]+)/$TORCHX_INSTANCE-[0-9]+|W\1|" \
+    | sed -uE 's/-[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}//' \
+    | awk '{sub(/^[[:digit:]]{4}-/,"",$2); sub(/T/," ",$2); sub(/\.[[:digit:]]+/,"",$2); printf "\x1b[33;1m%s \x1b[0;2m%s\x1b[0m", $1, $2; for (i=3; i<=NF; i++) printf " %s", $i; print ""; fflush()}' \
     | tee "${STREAMCONSUMER_LOGS}job.txt"
