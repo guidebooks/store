e12b4b98b869ace2eec62383e248e2a5e6413302
Nick Mitchell <nickm@us.ibm.com>
2023-04-04 10:57:20 -0400

    fix: don't fail if we can't hack uid-range

--- guidebooks/ml/ray/hacks/openshift/uid-range-update.sh
+++ guidebooks/ml/ray/hacks/openshift/uid-range-update.sh
@@ -1,2 +1,3 @@
 kubectl annotate namespace ${KUBE_NS} ${KUBE_CONTEXT_ARG} --overwrite openshift.io/sa.scc.uid-range=1000/10000 \
-    || oc annotate project ${KUBE_NS} ${KUBE_CONTEXT_ARG} --overwrite openshift.io/sa.scc.uid-range=1000/10000
+    || oc annotate project ${KUBE_NS} ${KUBE_CONTEXT_ARG} --overwrite openshift.io/sa.scc.uid-range=1000/10000 \
+    || echo "Unable to annotate uid-range"
