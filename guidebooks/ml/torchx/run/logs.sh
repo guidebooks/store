fd9983da97b6e9edb67997eafd003e6dfab25b4c
Nick Mitchell <nickm@us.ibm.com>
2023-04-04 20:56:26 -0400

    fix: sed RE error can occur in torchx log streamer

--- guidebooks/ml/torchx/run/logs.sh
+++ guidebooks/ml/torchx/run/logs.sh
@@ -9,6 +9,8 @@ kubectl logs -f \
         ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} \
         -l ${TORCHX_INSTANCE_LABEL} \
     | sed -uE "s|pod/$TORCHX_INSTANCE-([0-9]+)/$TORCHX_INSTANCE-[0-9]+|W\1|" \
-    | sed -uE 's/-[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}//' \
     | awk '{sub(/^[[:digit:]]{4}-/,"",$2); sub(/T/," ",$2); sub(/\.[[:digit:]]+/,"",$2); printf "\x1b[33;1m%s \x1b[0;2m%s\x1b[0m", $1, $2; for (i=3; i<=NF; i++) printf " %s", $i; print ""; fflush()}' \
     | tee "${STREAMCONSUMER_LOGS}job.txt"
+
+# used to be after first sed
+# | sed -uE 's/-[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}//' \
