953d67c8a6f80d923c98a43bfc5832544cb8c46f
Nick Mitchell <nickm@us.ibm.com>
2023-04-06 14:06:40 -0400

    fix: remove leftover 'set -x' from debugging

--- guidebooks/ml/torchx/run/status-poller.sh
+++ guidebooks/ml/torchx/run/status-poller.sh
@@ -4,7 +4,6 @@ if [ $(uname) = "Darwin" ]; then
     export REPLSIZE="-S5000"
 fi
 
-set -x
 kubectl get pods ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} -l ${TORCHX_INSTANCE_LABEL} -o name \
         --field-selector=status.phase==Running \
     | xargs ${REPLSIZE} -P128 -I {} -n1 \
