b7f795b4bb7c7b7c1e195d1acacd01a3a0d8b716
Nick Mitchell <nickm@us.ibm.com>
2023-04-10 10:32:47 -0400

    fix: vmstat data lacks pod/ prefix on pod name

--- guidebooks/ml/ray/run/pod-vmstat.sh
+++ guidebooks/ml/ray/run/pod-vmstat.sh
@@ -16,5 +16,5 @@ if [ -z "$STREAMCONSUMER_RESOURCES" ]; then STREAMCONSUMER_RESOURCES="/tmp/"; fi
 kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} -o name \
         --field-selector=status.phase==Running \
     | xargs ${REPLSIZE} -P128 -I {} -n1 \
-            sh -c "kubectl exec --pod-running-timeout=1h ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} {} -- sh -c \"TZ=UTC vmstat --timestamp 5 | awk -Winteractive -v pod=\\\$(hostname) '\\\$NF !~ /timestamp/ && \\\$3 != \\\"swpd\\\" {printf(\\\"\\\x1b[1;33m[CPU Utilization %4.1f%%] \\\x1b[0;33m%-30s \\\x1b[2m%14s %2s %2s %2s %2s %2s %sT%sZ\\\x1b[0m\\\n\\\", \\\$13+\\\$14, pod, \\\$4, \\\$13, \\\$14, \\\$15, \\\$16, \\\$17, \\\$(NF-1), \\\$NF)}'\"" 2> /dev/null \
+            sh -c "kubectl exec --pod-running-timeout=1h ${KUBE_CONTEXT_ARG} ${KUBE_NS_ARG} {} -- sh -c \"TZ=UTC vmstat --timestamp 5 | awk -Winteractive -v pod=\\\$(hostname) '\\\$NF !~ /timestamp/ && \\\$3 != \\\"swpd\\\" {printf(\\\"\\\x1b[1;33m[CPU Utilization %4.1f%%] \\\x1b[0;33mpod/%-30s \\\x1b[2m%14s %2s %2s %2s %2s %2s %sT%sZ\\\x1b[0m\\\n\\\", \\\$13+\\\$14, pod, \\\$4, \\\$13, \\\$14, \\\$15, \\\$16, \\\$17, \\\$(NF-1), \\\$NF)}'\"" 2> /dev/null \
     >> "${STREAMCONSUMER_RESOURCES}pod-vmstat.txt"
