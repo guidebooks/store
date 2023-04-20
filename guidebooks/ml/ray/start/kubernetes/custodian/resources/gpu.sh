b4de4cb268f3875c5ee75e3b9fff68639968c168
Nick Mitchell <nickm@us.ibm.com>
2023-04-20 13:32:35 -0400

    feat: add gpu utilization pod to custodian

---
+++ guidebooks/ml/ray/start/kubernetes/custodian/resources/gpu.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} -o name \
+        --field-selector=status.phase==Running \
+    | xargs -P512 -I {} -n1 \
+            sh -c 'kubectl exec {} -- sh -c "nvidia-smi --query-gpu=timestamp,utilization.gpu,utilization.memory,memory.total,temperature.gpu,name --format=csv,noheader -l 10" | awk -v pod={} -F, -f /tmp/gpu.awk'
