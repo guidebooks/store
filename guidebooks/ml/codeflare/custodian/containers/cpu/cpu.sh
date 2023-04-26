80d952d4bd4ef83ef5c51516336e4188051a4f57
Nick Mitchell <nickm@us.ibm.com>
2023-04-26 19:30:33 -0400

    fix: improve custodian memory requests for larger jobs
    We had been using a fixed amount of memory for the cpu/mem/gpu trackers. But these need memory per worker (since we connect to each worker).
    
    This also updates the `kubectl logs` in the logs container to pass in a `--max-logs-requests` that is set to num workers for the run.
    
    This also fixes some issues with the custodian vs torchx jobs, to avoid tracking worker status and runtime env setup (since, for now, those are ray-specific).

--- guidebooks/ml/codeflare/custodian/containers/cpu/cpu.sh
+++ guidebooks/ml/codeflare/custodian/containers/cpu/cpu.sh
@@ -3,4 +3,4 @@
 kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} -o name \
         --field-selector=status.phase==Running \
     | xargs -P512 -I {} -n1 \
-            sh -c 'kubectl exec {} -- sh -c "TZ=UTC vmstat --timestamp 5" | awk -v pod={} -f /tmp/cpu.awk'
+            sh -c 'kubectl exec {} -- sh -c "TZ=UTC vmstat --timestamp 5" | awk -v pod={} -f /tmp/cpu.awk' 2> /dev/null
