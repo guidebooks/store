c8a108502e8a3d40e5e4d8a241fbc9840f345da7
Nick Mitchell <nickm@us.ibm.com>
2023-05-11 14:05:58 -0400

    fix: cpu utilization information may be bogus; switch to cgroup-based stats
    This also adds a CPU Throttling metric.

--- guidebooks/ml/codeflare/custodian/containers/cpu/cpu.sh
+++ guidebooks/ml/codeflare/custodian/containers/cpu/cpu.sh
@@ -3,4 +3,4 @@
 kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} -o name \
         --field-selector=status.phase==Running \
     | xargs -P512 -I {} -n1 \
-            sh -c 'kubectl exec {} -- sh -c "TZ=UTC vmstat --timestamp 5" | awk -v pod={} -f /tmp/cpu.awk' 2> /dev/null
+            sh -c 'kubectl exec {} -- sh -c "while true; do echo \"timestamp \\\$(date -u)\"; echo \"nanos \\\$(date +%s%N)\"; cat /sys/fs/cgroup/cpu.stat; echo ------; sleep 1; done" | awk -v pod={} -f /tmp/cpu.awk'
