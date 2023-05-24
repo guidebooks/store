8ad9bef0877ea5af8384434c0576306560a6fafb
Nick Mitchell <nickm@us.ibm.com>
2023-05-24 19:28:09 -0400

    fix: more EOF protection fixes
    custodian should detect head pod existence
    logs/via-exec shouldn't bother to tee to local file

--- guidebooks/ml/codeflare/custodian/containers/mem/memory.sh
+++ guidebooks/ml/codeflare/custodian/containers/mem/memory.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} -o name \
-        --field-selector=status.phase==Running \
-    | xargs -P512 -I {} -n1 \
-            sh -c 'kubectl exec {} -- sh -c "while true; do echo \"\$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.current) \$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.max) \$(date -u +%Y-%m-%dT%H:%M:%SZ)\"; sleep 5; done" | awk -v pod={} -f /tmp/memory.awk' \
-            2> /dev/null
+while true; do
+    kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} -o name \
+            --field-selector=status.phase==Running \
+        | xargs -P512 -I {} -n1 \
+                sh -c 'kubectl exec {} -- sh -c "while true; do echo \"\$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.current) \$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes 2> /dev/null || cat /sys/fs/cgroup/memory.max) \$(date -u +%Y-%m-%dT%H:%M:%SZ)\"; sleep 5; done" | awk -v pod={} -f /tmp/memory.awk' \
+                2> /dev/null
+
+    if [[ "\$(kubectl get pod -l ${KUBE_PODFULL_LABEL_SELECTOR} 2>&1)" =~ "No resources found" ]]; then break; fi
+    sleep 1
+done
